79aaa583f374d0bba69d108c5ca9a57d8126a5c3
Exwii <joaopedrothe@gmail.com>
2021-12-05 14:27:09 -0300

    feat: logic

--- scripts/tree.js
+++ scripts/tree.js
@@ -1,13 +1,53 @@
 let health = document.getElementById("health")
+let qntfolha = 8
+let folhas = ["folha1","folha2","folha3","folha4","folha5","folha6","folha7","folha8"]
+let folha
+
+
+
+
+
 
 function hit(){
-    let health = document.getElementById("health")
+    health = document.getElementById("health")
     health.value -= Math.random() * 5;
+    random()
+    death()
 }
 
-function dead(){
-    let health = document.getElementById("health")
-    if (health <= 0); {
-        console.log('corno')
+
+function random() {
+        const random = Math.floor(Math.random() * folhas.length)
+        return folha = folhas[random]
+        
     }
-}
+
+
+
+
+ function death(){
+    if(health.value <= 90) {
+        document.getElementById(folha).style.display = "none"
+        document.getElementById(folha).style.display = "none"
+    }
+
+    if(health.value <= 60) {
+        document.getElementById(folha).style.display = "none"
+        document.getElementById(folha).style.display = "none"
+    }
+
+    if(health.value <= 40) {
+        document.getElementById(folha).style.display = "none"
+        document.getElementById(folha).style.display = "none"
+    }
+
+    if(health.value <= 20) {
+        document.getElementById(folha).style.display = "none"
+        document.getElementById(folha).style.display = "none"
+    }
+
+    if(health.value <= 0) {
+        document.getElementById("tronco").style.display = "none"
+    }
+} 
+
